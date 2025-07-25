bfbb60dcaa50d008317ef14534f0efe17a95ba34
liahrothstein <liahrothstein@gmail.com>
2025-07-25 16:57:09 +0300

    feat: add new back exercises

--- src/shared/lib/utils/set-back-class-name.ts
+++ src/shared/lib/utils/set-back-class-name.ts
@@ -54,7 +54,7 @@ export function setBackClassName(exercise: string): string {
         case 'Гиперэкстензия':
             return ('back hyperextension');
             break;
-        case 'Пуловер в кроссовере':
+        case 'Тяга Рейдера в кроссовере':
             return ('back pulloverInACrossover');
             break;
         case 'Пуловер в кроссовере широкая рукоятка':
@@ -131,6 +131,72 @@ export function setBackClassName(exercise: string): string {
             break;
         case 'Тяга вертикального блока узкая рукоятка':
             return ('back verticalBlockThrustNarrowHandle');
+            break;
+        case 'Австралийские подтягивания обратным хватом':
+            return ('back australianPullUpsWithReverseGrip');
+            break;
+        case 'Австралийские подтягивания обратным хватом со скрещенными ногами':
+            return ('back australianReverseGripPullUpsWithCrossedLegs');
+            break;
+        case 'Тяга к поясу в кроссовере рукоятки сидя':
+            return ('back beltPullInTheCrossoverArmsWhileSitting');
+            break;
+        case 'Приведение локтей в кроссовере сидя':
+            return ('back bringingElbowsInACrossoverWhileSitting');
+            break;
+        case 'Приведение локтя в кроссовере с верхнего блока':
+            return ('back bringingTheElbowIntoTheCrossoverFromTheUpperBlock');
+            break;
+        case 'Тяга к груди в кроссовере стоя':
+            return ('back chestTractionInAStandingCrossover');
+            break;
+        case 'Тяга в кроссовере сидя на горизонтальной скамье':
+            return ('back crossoverTractionWhileSittingOnAHorizontalBench');
+            break;
+        case 'Французский жим в кроссовере лёжа, канатной рукояткой, с нижнего блока':
+            return ('back frenchBenchPressInTheCrossoverRopeGripFromTheLowerBlock');
+            break;
+        case 'Тяга гантелей к поясу на наклонной скамье':
+            return ('back liftingDumbbellsToTheBeltOnAnInclinedBench');
+            break;
+        case 'Тяга одной рукой к поясу в Смите':
+            return ('back oneHandedBeltPullInSmith');
+            break;
+        case 'Тяга блина к поясу в наклоне':
+            return ('back pancakePullToTheBeltInATilt');
+            break;
+        case 'Тяга гантелей к поясу в наклоне, с упором головы о скамью':
+            return ('back pullDumbbellsToTheBeltInATiltWithTheHeadRestingOnTheBench');
+            break;
+        case 'Подтягивания с опорой на одну ногу':
+            return ('back pullUpsWithSupportOnOneLeg');
+            break;
+        case 'Шраги в Смите широким хватом за спину':
+            return ('back scarsInSmithWithAWideBackGrip');
+            break;
+        case 'Тяга одной рукоятки в кроссовере в пол амплитуды с опорой на вертикальную скамью':
+            return ('back theThrustOfOneHandleInTheCrossoverIsHalfTheAmplitudeSupportedByAVerticalBench');
+            break;
+        case 'Тяга горизонтального блока с небольшим наклоном корпуса вперед':
+            return ('back thrustOfAHorizontalBlockWithASlightForwardTiltOfTheBody');
+            break;
+        case 'Тяга в кроссовере канатной рукоятки, с верхнего блока':
+            return ('back tractionInTheCrossoverOfTheRopeHandleFromTheUpperBlock');
+            break;
+        case 'Тяга рукояток в кроссовере со сведением лопаток с опорой на вертикальную скамью':
+            return ('back tractionOfTheHandlesInACrossoverWithTheReductionOfTheBladesSupportedOnAVerticalBench');
+            break;
+        case 'Тяга горизонтального блока рукоятки к поясу с опорой на колено':
+            return ('back tractionOfTheHorizontalBlockOfTheHandleToTheBeltWithSupportOnTheKnee');
+            break;
+        case 'Тяга Рейдера в кроссовере рукояткой':
+            return ('back tractionOfTheRaiderInTheCrossoverWithTheHandle');
+            break;
+        case 'Тяга канатной рукоятки к поясу в кроссовере с нижнего блока':
+            return ('back tractionOfTheRopeHandleToTheBeltInTheCrossoverFromTheLowerBlock');
+            break;
+        case 'Тяга вертикального блока рукояткой':
+            return ('back tractionOfTheVerticalBlockByTheHandle');
             break;
         default:
             return ('back verticalBlockThrustWithNarrowThreadedGrip');
