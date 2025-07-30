d7153de0b356e53d23f971de04295c93cf47c35e
liahrothstein <liahrothstein@gmail.com>
2025-07-30 12:41:41 +0300

    feat: add switch show buttons

--- src/entities/stretching/ui/Stretching.tsx
+++ src/entities/stretching/ui/Stretching.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Button, Cascader, InputNumber, Typography } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { CaretDownOutlined, CaretRightOutlined, PlusOutlined } from '@ant-design/icons';
 
 import { StretchingPreview } from '@components/stretching-preview';
 
@@ -17,39 +17,49 @@ export function Stretching() {
     const [exercise, setExercise] = useState<string[] | null | undefined>(null);
     const [attempts, setAttempts] = useState<number | null>(null);
     const [times, setTimes] = useState<number | null>(null);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     const { Title } = Typography;
 
     return (
         <div className='stretching'>
-            <Title level={4}>Растяжка</Title>
-            <div>
-                <Cascader
-                    onChange={(e) => { setExercise(e) }}
-                    options={stretchingCascaderProps.options}
-                    placeholder={stretchingCascaderProps.placeholder}
-                    className='stretchingType' />
-                <InputNumber
-                    min={1}
-                    max={100}
-                    placeholder='Подходы'
-                    onChange={(e) => { setAttempts(e) }}
-                    className='attempts' />
-                <InputNumber
-                    min={1}
-                    max={100}
-                    placeholder='Повторения'
-                    onChange={(e) => { setTimes(e) }}
-                    className='times' />
+            <div className="titleWithButton">
+                <Button
+                    icon={(isOpen) ? <CaretDownOutlined /> : <CaretRightOutlined />}
+                    onClick={() => { setIsOpen(!isOpen) }} />
+                <Title level={4} className='stretching'>Растяжка</Title>
             </div>
-            <StretchingPreview exercise={exercise} />
-            <Button
-                icon={<PlusOutlined />}
-                disabled={!exercise}
-                color='green'
-                variant='solid'
-                onClick={() => { dispatch(setStretching([...stretching, { exercise: exercise, attempts: attempts, times: times }])) }}
-                className='stretching'>Добавить упражнение</Button>
+            {(isOpen) &&
+                <>
+                    <div>
+                        <Cascader
+                            onChange={(e) => { setExercise(e) }}
+                            options={stretchingCascaderProps.options}
+                            placeholder={stretchingCascaderProps.placeholder}
+                            className='stretchingType' />
+                        <InputNumber
+                            min={1}
+                            max={100}
+                            placeholder='Подходы'
+                            onChange={(e) => { setAttempts(e) }}
+                            className='attempts' />
+                        <InputNumber
+                            min={1}
+                            max={100}
+                            placeholder='Повторения'
+                            onChange={(e) => { setTimes(e) }}
+                            className='times' />
+                    </div>
+                    <StretchingPreview exercise={exercise} />
+                    <Button
+                        icon={<PlusOutlined />}
+                        disabled={!exercise}
+                        color='green'
+                        variant='solid'
+                        onClick={() => { dispatch(setStretching([...stretching, { exercise: exercise, attempts: attempts, times: times }])) }}
+                        className='stretching'>Добавить упражнение</Button>
+                </>
+            }
         </div>
     )
 }
